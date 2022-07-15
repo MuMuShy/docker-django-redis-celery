801617eb2cacc46fee439aed07b44fd047b1d12e
mumu <mumushy850421@gmail.com>
2022-07-15 16:13:33 +0800

    update

--- app/app/urls.py
+++ app/app/urls.py
@@ -6,6 +6,7 @@ from core import views as todoapi_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('callback/', todoapi_views.BaseCallbackView.as_view()),
     path('api/task/', todoapi_views.TaskList.as_view(), name='task-list'),
     path('api/task/<int:task_id>/', todoapi_views.TaskDetail.as_view(), name='task-detail'),
 ]
